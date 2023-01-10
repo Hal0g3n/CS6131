caf790382f5930fde39875aab798da89ec101e72
Haloen <72333894+Haloen@users.noreply.github.com>
2023-01-10 14:22:05 +0800

    ok fixed the dumb issue

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("@vue/cli-service");
+
+module.exports = defineConfig({
+    publicPath: process.env.NODE_ENV == "production" ? "/CS6131/" : "/",
+    lintOnSave: false,
+    transpileDependencies: [
+        "vuetify"
+    ],
+    chainWebpack: (config) => {
+        config.module
+            .rule("ts")
+            .use("ts-loader")
+            .loader("ts-loader");
+    },
+});
